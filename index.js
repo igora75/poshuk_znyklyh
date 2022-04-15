94509792ccbc860f3e54653fe78efe57cbae9528
igora75 <igora75@gmail.com>
2022-04-15 07:48:30 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import Post from "./Post.js";
+import router from "./router.js";
+
+const PORT = 5000
+const DB_URL = 'mongodb+srv://user:user@cluster0.vigie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log('SERVER STARTER'))
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+startApp()
