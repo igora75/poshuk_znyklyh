74d1811938247843835ebff4b56ead102cbbbe5a
igora75 <igora75@gmail.com>
2022-04-16 17:48:35 +0300

    add pictures

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./router.js";
+import fileUpload from "express-fileupload"
 
 const PORT = 3000
 const DB_URL = 'mongodb+srv://user:user@cluster0.vigie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -8,6 +9,8 @@ const DB_URL = 'mongodb+srv://user:user@cluster0.vigie.mongodb.net/myFirstDataba
 const app = express()
 
 app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 
