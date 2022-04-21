1aa606ffa166c272e0aacdff27843aa7e4239c2b
igora75 <igora75@gmail.com>
2022-04-21 21:17:00 +0300

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./router.js";
 import fileUpload from "express-fileupload"
+import cors from "cors"
 
 const PORT = 3000
 const DB_URL = 'mongodb+srv://user:user@cluster0.vigie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
